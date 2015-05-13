7d49e5725432b3fa0ff25bcd8b0cbcf0f4bdbd87
luckystar888 <taotiffany88@gmail.com>
2015-05-13 12:26:30 -0500

    test

---
+++ src/evidence/Interrogation.java
@@ -0,0 +1,37 @@
+package evidence;
+
+public class Interrogation 
+{
+	private static String[] weapons = {"The Norton","Gun Candlestick","Tuba","Frozen Waterbottle","Cafeteria Food","Fire Axe"};
+	private static String[] people = {
+		"Nicholas Biegel",
+		"Victor Bolivar",
+		"Nathon Bowdish",
+		"Jordan Buckmaster",
+		"Jocelynn Cheesebourough",
+		"Jay Chopra",
+		"Daniel Gilbert",
+		"Lauren Granskog",
+		"Ethan Hunt",
+		"Samuel Hupp",
+		"Nathon Jackson",
+		"Jacob Kieta",
+		"Celine McCormack",
+		"Noah Miller",
+		"Mike the Pimp Norcutt",
+		"Gage O'Conner",
+		"Mathew Palm",
+		"Dino Rinaldi",
+		"Andrew Ring",
+		"Peter Shepered",
+		"Justin Tancos",
+		"Tiffany Tao",
+		"Jesse Veloz",
+		"Jakub Wrobel",
+		"Jacob Zak"
+	};
+	private static String finalAnswer = weapons[(int)(Math.random() * 6)] + " " + people[(int)(Math.random() * 25) + " " + listOfRooms[(int)(Math.random() * 9)];
+	}
+
+
+}
