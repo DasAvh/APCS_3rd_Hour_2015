273a4061ee29d85ad22693526365f328019ae75a
Norcutt39 <Rsrecover99@gmail.com>
2015-05-13 12:04:09 -0500

    Added Names

--- src/evidence/Testing.java
+++ src/evidence/Testing.java
@@ -3,13 +3,33 @@ package evidence;
 public class Testing {
 	private static String[] weapons = {"The Norton","Gun Candlestick","Tuba","Frozen Waterbottle","Cafeteria Food","Fire Axe"};
 	private static String[] people = {
-		"",
-		"",
-		""
+		"Nicholas Biegel",
+		"Victor Bolivar",
+		"Nathon Bowdish",
+		"Jordan Buckmaster",
+		"Jocelynn Cheesebourough",
+		"Jay Chopra",
+		"Daniel Gilbert",
+		"Lauren Granskog",
+		"Ethan Hunt",
+		"Samuel Hupp",
+		"Nathon Jackson",
+		"Jacob Kieta",
+		"Celine McCormack",
+		"Noah Miller",
+		"Mike the Pimp Norcutt",
+		"Gage O'Conner",
+		"Mathew Palm",
+		"Dino Rinaldi",
+		"Andrew Ring",
+		"Peter Shepered",
+		"Justin Tancos",
+		"Tiffany Tao",
+		"Jesse Veloz",
+		"Jakub Wrobel",
+		"Jacob Zak"
 	};
 	public static void main(String[] args){
-		for(String i : weapons){
-			System.out.println(i);
-		}
+
 	}
 }
